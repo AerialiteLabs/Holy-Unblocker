ab178a7e79fa7a58bf891d53a31ce3bed4b5071f
Sophia <soap-phia@noreply.github.com>
2025-11-06 11:42:42 -0500

    dist

---
+++ views/dist/uv/sw.js
@@ -0,0 +1,3 @@
+(()=>{importScripts("https://cdn.jsdelivr.net/gh/AerialiteLabs/holy-unblocker@6.9.3/views/uv/uv.bundle.js");importScripts("/uv/uv.config.js");importScripts(self[`g6	h39n]2Dl1<H4Q
+Z3Sr%1`].sw||"https://cdn.jsdelivr.net/gh/AerialiteLabs/holy-unblocker@6.9.3/views/uv/uv.sw.js");var t=new UVServiceWorker;self.addEventListener("fetch",r=>{r.respondWith((async()=>t.route(r)?await t.fetch(r):await fetch(r.request))())});})();
+//# sourceMappingURL=sw.js.map
